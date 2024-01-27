7480a3030d7875ff898943eac2bc9902c01efd7c
Przemysław Rutkowski <rutkowski_przemyslaw@o2.pl>
2024-01-27 13:00:33 +0100

    Add navigation from login to panel with reports

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 import { login } from '../services/AuthService'
+import { useNavigate } from "react-router-dom";
 import '../styles/login.css'
 
 const LoginForm: React.FC = () => {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 
+	const navigation = useNavigate();
+
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault()
 		try {
 			const user = await login(email, password)
+			navigation('/reports')
 			console.log(user)
 		} catch (error) {
 			console.error(error)
@@ -21,7 +25,7 @@ const LoginForm: React.FC = () => {
 			<form onSubmit={handleSubmit}>
 				<p>Computer Service</p>
 				<div>
-                <label>Email</label>
+					<label>Email</label>
 					<input type="email" value={email} onChange={e => setEmail(e.target.value)} />
 				</div>
 				<div>
